13f3875379cdc12540b0527201000d8a0fa6d5a5
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-24 23:01:49 -0300

    up

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Page About</h1>
+        <NuxtLink to="/">Acessar</NuxtLink>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "About"
+}
+</script>
+
+<style scoped>
+    h1 {
+        color: rgb(0, 11, 73);
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <NuxtLink to="/about">Acessar</NuxtLink>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+    h1 {
+        color: aqua;
+    }
+</style>

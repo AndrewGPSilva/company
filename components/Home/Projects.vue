5755defcef9479ffa457e47321af8dba5ac2e18c
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-26 23:06:17 -0300

    componente para exibir alguns projetos em andamento

---
+++ components/Home/Projects.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="container">
+        <p>
+            "Construindo o futuro da <br>
+            web, hoje. Tecnologia de <br>
+            ponta, sites de excelência."
+        </p>
+        <div class="containerImagens">
+            <div class="imagemPost">
+                <img :src="srcImagem" :alt="altImagem" />
+            </div>
+            <div class="imagemTech">
+                <img :src="srcIconUm" :alt="altIconUm" />
+                <img :src="srcIconDois" :alt="altIconDois" />
+                <img :src="srcIconTres" :alt="altIconTres" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Projects",
+    props: {
+        srcImagem: {
+            type: String as () => string,
+            required: true
+        },
+        altImagem: {
+            type: String as () => string,
+            required: true
+        },
+        srcIconUm: {
+            type: String as () => string,
+            required: true
+        },
+        srcIconDois: {
+            type: String as () => string,
+            required: true
+        },
+        srcIconTres: {
+            type: String as () => string,
+            required: true
+        },
+        altIconUm: {
+            type: String as () => string,
+            required: true
+        },
+        altIconDois: {
+            type: String as () => string,
+            required: true
+        },
+        altIconTres: {
+            type: String as () => string,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    padding-left: 20px;
+    padding-right: 20px;
+    font-size: 20px;
+    padding-top: 5px;
+}
+
+p {
+    color: #545454;
+    font-family: 'Roboto Mono', monospace;
+}
+
+.containerImagens {
+    display: flex;
+    justify-content: center;
+    padding: 20px;
+    gap: 20px;
+}
+
+.imagemPost img {
+    border-radius: 30px;
+    height: 170px;
+    width: 250px;
+}
+
+.imagemTech {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-left: 1px solid black;
+}
+
+.imagemTech img {
+    margin-left: 10px;
+    width: 55px;
+    height: 55px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,16 @@
         src="https://i.ibb.co/bHhV9zC/postDois.webp" alt="Imagem do Segundo Post" lado="left"
         />
         <HomeBtnContact />
+        <HomeProjects 
+            srcImagem="https://m.media-amazon.com/images/I/B13h1EXqDiS.jpg" 
+            altImagem="Imagem da tela inicial da Netflix" 
+            srcIconUm="https://i.ibb.co/f27wdz5/spring.png"
+            altIconUm="Icone da logo do framework Spring" 
+            srcIconDois="https://i.ibb.co/WnqxL8d/html.png"
+            altIconDois="Icone da logo do HTML"  
+            srcIconTres="https://i.ibb.co/bFXpr1R/css.png"
+            altIconTres="Icone da logo do CSS" 
+        />
     </main>
 </template>
 
